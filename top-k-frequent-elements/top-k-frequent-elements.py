58865f0eccd28ed233bea6c3df7a206ba452f889
harshitlohani19 <harshitlohani2002@gmail.com>
2024-10-02 21:41:16 +0530

    top-k-frequent elements

---
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,13 @@
+from collections import Counter
+def topKFrequent(nums, k):# pass
+
+    counter = Counter(nums)
+    a=[]
+    b=counter.most_common(k)
+    for i in b:
+        a.append(i[0])
+    print(a)
+
+
+if __name__ == '__main__':
+    topKFrequent([1,1,1,2,2,3],2)
